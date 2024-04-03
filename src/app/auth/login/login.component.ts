a3bef6a603de752cfee1e985f0cfda4e4bde5a73
mohsenXAD <mohsenxad@gmail.com>
2024-04-03 15:49:55 +0330

    check how deveopment section of github works #58

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent {
+@Component(
+	{
+		selector: 'app-login',
+		templateUrl: './login.component.html',
+		styleUrls: ['./login.component.scss']
+	}
+)
 
-}
+export class LoginComponent
+	{
+
+	}
